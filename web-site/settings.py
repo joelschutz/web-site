441ced8c4957b07f179e3575d1c08da09929b8d1
joelschutz <joeymarley@gmail.com>
2020-07-15 16:58:17 -0300

    fixing staticfiles

--- web-site/settings.py
+++ web-site/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'web-site.urls'
@@ -118,8 +119,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT="/var/www/joelschutz.herokuapp.com/static/"
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'portifolio/static'),
+    os.path.join(BASE_DIR, 'que_nome/static'),
+    os.path.join(BASE_DIR, 'umafoto_ae/static'),
+)
