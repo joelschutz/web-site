284c266099f998486cd9685464239f39a8c983eb
joelschutz <joeymarley@gmail.com>
2020-07-17 01:51:05 -0300

    domain new

--- web_site/settings.py
+++ web_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['joelschutz.com.br', 'localhost', '127.0.0.1', 'joelschutz.herokuapp.com', 'www.joelschutz.herokuapp.com']
+ALLOWED_HOSTS = ['joelschutz.com.br', 'localhost', '127.0.0.1', 'joelschutz.herokuapp.com', 'www.joelschutz.com']
 
 
 # Application definition
