581e11cc000388092b41d2b14f333334df534b57
joelschutz <joeymarley@gmail.com>
2020-07-20 03:05:37 -0300

    Refactoring photohunter

--- umafoto_ae/views.py
+++ umafoto_ae/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 from umafoto_ae.photohunter import PhotoHunter
 
 
-def home(request): return render(request, 'pages/umafoto_ae_home.html')
+def home(request): 
+    return render(request, 'pages/umafoto_ae_home.html')
 
 
-def info(request): return render(request, 'pages/umafoto_ae_info.html')
+def info(request): 
+    return render(request, 'pages/umafoto_ae_info.html')
 
 
 def fotos(request):
