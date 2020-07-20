aec9e0e7aa270542a8c3382bd86fe1c527cd3451
joelschutz <joeymarley@gmail.com>
2020-07-20 03:51:10 -0300

    Refactoring photohunter

--- umafoto_ae/photohunter.py
+++ umafoto_ae/photohunter.py
@@ -23,8 +23,11 @@ class PhotoHunter:
         except IndexError:
             return {'status': False}
 
+
 class ResponseProcesser:
     """This class is responsable for interpreting and storing the API Response."""
+    
+    jsons_storage='./umafoto_ae/jsons/'
 
     def fill_photo_info(self, photo_form, ph: PhotoHunter):
         if ph.provider == 'unsplash':
@@ -58,22 +61,21 @@ class ResponseProcesser:
     def pick_photo(self, ph: PhotoHunter):
 
         try:
-            with open(f'./umafoto_ae/jsons/{ph.provider}_{ph.query}_{ph.orientation}_{ph.color}.json', 'rt') as file:
+            with open(f'{self.jsons_storage}{ph.provider}_{ph.query}_{ph.orientation}_{ph.color}.json', 'rt') as file:
                 response = json.load(file)
                 print('Entregando JSON armazenado')
         except FileNotFoundError:
             response = ApiGetter(ph).get_response()
             print('Entregando JSON novo')
             self.store_json(response, ph)
-        except Exception as e:
-            print(e)
-        finally:
-            print('Escolhendo Foto')
-            photo = self.pick_random_photo(response, ph)
-            self.fill_photo_info(photo, ph)
+        
+        
+        print('Escolhendo Foto')
+        photo = self.pick_random_photo(response, ph)
+        self.fill_photo_info(photo, ph)
 
     def store_json(self, json_form, ph: PhotoHunter):
-        with open(f'./umafoto_ae/jsons/{ph.api_provider}_{ph.query}_{ph.orientation}_{ph.color}.json', 'at') as file:
+        with open(f'{self.jsons_storage}{ph.provider}_{ph.query}_{ph.orientation}_{ph.color}.json', 'at') as file:
             json.dump(json_form, file)
 
 class ApiGetter:
@@ -111,8 +113,8 @@ class ApiGetter:
             "white": "white",
             "black": "black"}
         query = f'&query={self.query}' if self.query else '&query=photo'
-        color = f'&color={colors[self.color]}' if self.color is not 'all' else ''
-        orientation = f'&orientation={orientations[self.orientation]}' if self.orientation is not 'all' else ''
+        color = f'&color={colors[self.color]}' if self.color != 'all' else ''
+        orientation = f'&orientation={orientations[self.orientation]}' if self.orientation != 'all' else ''
         api_request = ''.join(['https://api.unsplash.com/search/photos', os.environ.get(
             "UNSPLASH_KEY"), self.PER_PAGE, query, orientation, color])
         response = get(api_request)
@@ -131,7 +133,7 @@ class ApiGetter:
         return self.prepare_response(response)
 
     def prepare_response(self, response):
-        if response.status_code is not 200:
+        if response.status_code != 200:
             raise ServiceNotFound('Resposta inesperada do servidor')
         else:
             self.request_limit[self.provider] = response.headers['X-RateLimit-Remaining']
@@ -139,7 +141,8 @@ class ApiGetter:
 
     def get_response(self):
         if self.limit > 5:
-            return eval(self.provider.lower())()
+            
+            return eval(f'self.{self.provider}')()
         else:
             raise ReachingAPILimit('The API is near its limits.')
 
