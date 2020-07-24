f8294424727e22e37617c4dab4d4ae019dad16b3
joelschutz <joeymarley@gmail.com>
2020-07-24 01:00:39 -0300

    Finishing homepage

--- home/blocks.py
+++ home/blocks.py
@@ -5,8 +5,9 @@ class CourseBlock(blocks.StructBlock):
     """Block for courses"""
 
     institution = blocks.CharBlock(required=True, help_text="Intitutição")
-    courses = blocks.ListBlock(blocks.CharBlock(required=True, help_text="Curso", icon='plus'),
-     required=True)
+    courses = blocks.ListBlock(
+        blocks.CharBlock(required=True, help_text="Curso", icon='plus'),
+        required=True)
     note = blocks.CharBlock(required=True, help_text="Descreva o estado ou nota final")
     begining_date = blocks.DateBlock(required=True, help_text="Data de Incio")
     finishing_date = blocks.DateBlock(required=False, help_text="Data de Término")
@@ -21,7 +22,7 @@ class FreeCourseBlock(blocks.StructBlock):
     """Block for free courses"""
 
     institution = blocks.CharBlock(required=True, help_text="Intitutição")
-    courses = blocks.ListBlock(blocks.CharBlock(required=True, help_text="Curso"))
+    courses = blocks.ListBlock(blocks.CharBlock(required=True, help_text="Curso", icon='plus'))
 
 
     class Meta:  # noqa
@@ -43,3 +44,28 @@ class JobBlock(blocks.StructBlock):
         template = "streams/job_block.html"
         icon = "fa-building"
         label = "Experiência profissional"
+
+class ProjectBlock(blocks.StructBlock):
+    """Block for projects"""
+
+    state = blocks.ChoiceBlock(
+        required=True,
+        help_text="Estado do desenvolvimento",
+        choices=[
+        ('developing', 'Em desenvolvimento'),
+        ('done', 'Pronto'),
+        ('deployed', 'Deployed'),
+        ('terminated', 'Fora do Ar'),
+        ])
+    descrition = blocks.CharBlock(required=True, help_text="Descrição do projeto")
+    links = blocks.ListBlock(blocks.StructBlock([
+        ('text', blocks.CharBlock(required=False, help_text="Nome do Destino")),
+        ('url', blocks.URLBlock(required=False, help_text="URL do Destino")),
+
+    ], icon='link'))
+
+
+    class Meta:  # noqa
+        template = "streams/project_block.html"
+        icon = "fa-cubes"
+        label = "Projeto"
